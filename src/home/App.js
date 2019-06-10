d4937dc3900d6c09c4683e5ef76968ed1c3cdaee
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-06-10 11:00:06 -0700

    created App component

---
+++ src/home/App.js
@@ -0,0 +1,27 @@
+import Component from '../Component.js';
+import Header from '../shared/Header.js';
+
+class App extends Component {
+
+    render() {
+        const dom = this.renderDOM();
+        const main = dom.querySelector('main');
+
+        const header = new Header();
+        dom.insertBefore(header.render(), main);
+
+
+
+        return dom;
+    }
+
+    renderTemplate() {
+        return /*html*/ `
+            <div>
+                <main></main>
+            </div>
+    `;
+    }
+}
+
+export default App;

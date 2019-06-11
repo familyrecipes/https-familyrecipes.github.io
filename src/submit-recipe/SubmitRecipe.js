e0528ad826366b091f9722a1712b80e93b5de95e
Erin Gurley <erinmgurley@gmail.com>
2019-06-11 10:04:49 -0700

    reworked form for more inputs

--- src/submit-recipe/SubmitRecipe.js
+++ src/submit-recipe/SubmitRecipe.js
@@ -24,7 +24,13 @@ class SubmitRecipe extends Component {
                 cookTime: formData.get('cook-time'),
                 readyIn: formData.get('ready-in'),
                 servings: formData.get('servings'),
-                ingredients: formData.get('ingredients')
+                ingredients: formData.get('ingredients'),
+                breakfast: formData.get('breakfast'),
+                lunch: formData.get('lunch'),
+                dinner: formData.get('dinner'),
+                dessert: formData.get('dessert'),
+                side: formData.get('side')
+
             };
 
             recipeRef.set(newRecipe).then(() => {
@@ -45,6 +51,19 @@ class SubmitRecipe extends Component {
                     <textarea name="description" placeholder="Describe your dish..."></textarea>
                     <label><input name="image-upload" placeholder="image uplaod">image upload</label>
                 </div>
+                    <input id="breakfast" type="checkbox" name="breakfast">Breakfast
+                    <input id="lunch" type="checkbox" name="lunch">Lunch
+                    <input id="dinner" type="checkbox" name="dinner">Dinner
+                    <input id="dessert" type="checkbox" name="dessert">Dessert
+                    <input id="side" type="checkbox" name="side">Side
+                <div>
+                    <select>Diet Type
+                        <option disabled selected value> select an option below </option>
+                        <option>Vegetarian</option>
+                        <option>Vegan</option>
+                        <option>Gluten-Free</option>
+                    </select>
+                </div>
                 <div>
                     <input name="prep-time" placeholder="Prep time...">
                     <input name="cook-time" placeholder="Cook time...">
@@ -52,11 +71,15 @@ class SubmitRecipe extends Component {
                     <input name="servings" placeholder="Servings...">
                 </div>
                 <div>
-                    <textarea name="ingredients"></textarea>
+                    <label>Ingredients
+                    <textarea name="ingredients" placeholder="1tbsp sugar, 3 cups flour..."></textarea>
+                    </label>
                 </div>
                 <div>
                     <textarea name="instructions" placeholder="Instructions..."></textarea>
                     <textarea name="notes" placeholder="Notes..."></textarea>
+                </div>
+                <div>
                 </div>
                 <button id="submit-button">Add recipe</button>
             </form>

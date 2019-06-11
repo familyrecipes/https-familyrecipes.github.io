83a966af6356e6e52eb4fb841ee0f7f02540a837
Erin Gurley <erinmgurley@gmail.com>
2019-06-11 10:52:48 -0700

    created recipe list folders, static and basic styling

--- src/recipe-list/RecipeList.js
+++ src/recipe-list/RecipeList.js
@@ -3,7 +3,20 @@ import Component from '../Component.js';
 class RecipeList extends Component {
     renderTemplate() {
         return /*html*/ `
-            
+            <ul id="recipe-list">
+                <li>
+                    <a href="">
+                        <h3>Bananas</h3>
+                        <img src="">
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <h3>Apples</h3>
+                        <img src="">
+                    </a>
+                </li>
+            </ul> 
     `;
     }
 }
---
+++ src/recipe-list/RecipeListApp.js
@@ -0,0 +1,28 @@
+import Component from '../Component.js';
+import Header from '../shared/Header.js';
+import RecipeList from './RecipeList.js';
+
+class RecipeListApp extends Component {
+    render() {
+        const dom = this.renderDOM();
+        const main = dom.querySelector('main');
+
+        const header = new Header();
+        dom.insertBefore(header.render(), main);
+
+        const recipeList = new RecipeList();
+        dom.appendChild(recipeList.render());
+
+        return dom;
+    }
+
+    renderTemplate() {
+        return /*html*/`
+            <div>
+                <main></main>
+            </div>
+        `;
+    }
+}
+
+export default RecipeListApp;

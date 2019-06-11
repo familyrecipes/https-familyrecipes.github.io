e3db4d1c38f46bf9e488bd5e6bda726144f2ad37
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-06-10 17:05:14 -0700

    created list and item components

---
+++ src/recipe-list/RecipeItem.js
@@ -0,0 +1,11 @@
+import Component from '../Component.js';
+
+class RecipeItem extends Component {
+    renderTemplate() {
+        return /*html*/ `
+            
+    `;
+    }
+}
+
+export default RecipeItem;
---
+++ src/recipe-list/RecipeList.js
@@ -0,0 +1,11 @@
+import Component from '../Component.js';
+
+class RecipeList extends Component {
+    renderTemplate() {
+        return /*html*/ `
+            
+    `;
+    }
+}
+
+export default RecipeList;

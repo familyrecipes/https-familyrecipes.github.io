9773865b1f47e24766e09ec2ce6f839ac7352851
Erin Gurley <erinmgurley@gmail.com>
2019-06-11 11:51:56 -0700

    added static info for test

--- src/recipe-list/RecipeList.js
+++ src/recipe-list/RecipeList.js
@@ -4,18 +4,7 @@ class RecipeList extends Component {
     renderTemplate() {
         return /*html*/ `
             <ul id="recipe-list">
-                <li>
-                    <a href="">
-                        <h3>Bananas</h3>
-                        <img src="">
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        <h3>Apples</h3>
-                        <img src="">
-                    </a>
-                </li>
+            
             </ul> 
     `;
     }

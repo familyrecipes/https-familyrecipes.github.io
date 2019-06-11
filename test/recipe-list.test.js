d2e6474f5362e05e861c4ca70a3696e094e232b1
Erin Gurley <erinmgurley@gmail.com>
2019-06-11 11:50:17 -0700

    created recipe list test

---
+++ test/recipe-list.test.js
@@ -0,0 +1,30 @@
+import RecipeItem from '../src/recipe-list/RecipeItem.js';
+
+const test = QUnit.test;
+QUnit.module('Recipe Item test'); 
+
+test('renders li', assert => {
+    //arrange
+
+    const newRecipeItem = new RecipeItem ({}); 
+
+    const expected = /*html*/ `
+        <li>
+            <a href="">
+                <h3>Bananas</h3>
+                <img src="">
+            </a>
+        </li>
+        <li>
+            <a href="">
+                <h3>Apples</h3>
+                <img src="">
+            </a>
+        </li>
+    `;
+
+    //act
+    const rendered = newRecipeItem.renderTemplate();
+    //assert
+    assert.htmlEqual(rendered, expected);
+});

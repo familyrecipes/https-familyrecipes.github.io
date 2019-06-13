36637af73911aa45d19913a384978b0d9dfd5525
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-06-13 16:04:51 -0700

    travis firebase yes

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -8,8 +8,7 @@ const config = {
     appId: '1:759581411685:web:3a3a4243b44afda7'
 };
 
-export const app = firebase.initializeApp(config);
-
+const app = firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 export const db = firebase.database();
@@ -20,3 +19,5 @@ export const allRecipesRef = db.ref('all-recipes');
 export const recipesByUserRef = db.ref('recipes-by-user');
 export const userFavoritesRef = db.ref('user-favorites');
 export const imageStorageRef = storage.ref('images');
+
+export default app;
